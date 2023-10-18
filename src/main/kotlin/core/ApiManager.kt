4b0d506e6da785bf7ed9c973102c7512c97d8687
pooyaj21 <105147486+pooyaj21@users.noreply.github.com>
2023-10-18 15:43:53 +0330

    use @Path instead of @Url

--- src/main/kotlin/core/ApiManager.kt
+++ src/main/kotlin/core/ApiManager.kt
@@ -3,21 +3,17 @@ package core
 import ApiLocationData
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import retrofit2.Retrofit
-import retrofit2.create
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
-import retrofit2.http.Url
 
 object ApiManager {
-
+    val json = Json {
+        ignoreUnknownKeys = true
+    }
     suspend fun weatherApiCreatorBaseOnCity(cityName: String): ApiWeatherData {
-        val json = Json {
-            ignoreUnknownKeys = true
-        }
-
         val api: WeatherApiService = Retrofit.Builder()
             .baseUrl("https://api.openweathermap.org")
             .addConverterFactory(json.asConverterFactory("application/json".toMediaTypeOrNull()!!))
@@ -26,33 +22,6 @@ object ApiManager {
 
         return api.getData(cityName, "9a553da7016360c1f1e8f07fdf39012b")
     }
-    suspend fun pollutionApiCreator(previousApi: ApiWeatherData): ApiPollutionData {
-        val json = Json {
-            ignoreUnknownKeys = true
-        }
-
-        val api: PollutionApiService = Retrofit.Builder()
-            .baseUrl("https://api.openweathermap.org")
-            .addConverterFactory(json.asConverterFactory("application/json".toMediaTypeOrNull()!!))
-            .build()
-            .create(PollutionApiService::class.java)
-
-        return api.getData(previousApi.coord.lat.toString(), previousApi.coord.lon.toString(), "9a553da7016360c1f1e8f07fdf39012b")
-    }
-
-
-    suspend fun weatherApiCreatorBaseOnLocation(ip:String): ApiWeatherData {
-        val baseUrl = "http://api.ipstack.com/$ip/"
-        val api:LocationApiService = Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .addConverterFactory(Json.asConverterFactory("application/json".toMediaTypeOrNull()!!))
-            .build()
-            .create(LocationApiService::class.java)
-
-        val city = api.getData(baseUrl,"184e07193f66719f366544d1c50e549b").city
-        return weatherApiCreatorBaseOnCity(city)
-    }
-
 
     interface WeatherApiService {
         @GET("/data/2.5/weather")
@@ -64,7 +33,21 @@ object ApiManager {
     }
 
 
-    interface PollutionApiService{
+    suspend fun pollutionApiCreator(previousApi: ApiWeatherData): ApiPollutionData {
+        val api: PollutionApiService = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org")
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaTypeOrNull()!!))
+            .build()
+            .create(PollutionApiService::class.java)
+
+        return api.getData(
+            previousApi.coord.lat.toString(),
+            previousApi.coord.lon.toString(),
+            "9a553da7016360c1f1e8f07fdf39012b"
+        )
+    }
+
+    interface PollutionApiService {
         @GET("/data/2.5/air_pollution")
         suspend fun getData(
             @Query("lat") lat: String,
@@ -73,10 +56,22 @@ object ApiManager {
         ): ApiPollutionData
     }
 
-    interface LocationApiService{
-        @GET
+
+    suspend fun weatherApiCreatorBaseOnLocation(ip: String): ApiWeatherData {
+        val api: LocationApiService = Retrofit.Builder()
+            .baseUrl("http://api.ipstack.com")
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaTypeOrNull()!!))
+            .build()
+            .create(LocationApiService::class.java)
+
+        val city = api.getData(ip, "184e07193f66719f366544d1c50e549b").city
+        return weatherApiCreatorBaseOnCity(city)
+    }
+
+    interface LocationApiService {
+        @GET("/{ip}")
         suspend fun getData(
-            @Url url: String,
+            @Path("ip") ip: String,
             @Query("access_key") accessKey: String
         ): ApiLocationData
     }
