4cb7a724f9b0ea4669e6650f546476184c3c6b9d
pooyaj21 <105147486+pooyaj21@users.noreply.github.com>
2023-10-19 17:29:49 +0330

    request with okHttp added

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("com.google.code.gson:gson:2.8.9")
 }
 
 tasks.test {
--- src/main/kotlin/core/ApiManager.kt
+++ src/main/kotlin/core/ApiManager.kt
@@ -1,9 +1,12 @@
 package core
 
 import ApiLocationData
+import com.google.gson.Gson
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -13,6 +16,7 @@ object ApiManager {
     val json = Json {
         ignoreUnknownKeys = true
     }
+
     suspend fun weatherApiCreatorBaseOnCity(cityName: String): ApiWeatherData {
         val api: WeatherApiService = Retrofit.Builder()
             .baseUrl("https://api.openweathermap.org")
@@ -44,7 +48,28 @@ object ApiManager {
             previousApi.coord.lat.toString(),
             previousApi.coord.lon.toString(),
             "9a553da7016360c1f1e8f07fdf39012b"
-        )
+        )                                                   //With retrofit
+
+
+//        val client = OkHttpClient()
+//        val gson = Gson()
+//
+//        val url = "https://api.openweathermap.org/data/2.5/air_pollution" +
+//                "?lat=${previousApi.coord.lat}&lon=${previousApi.coord.lon}&appid=9a553da7016360c1f1e8f07fdf39012b"
+//
+//        val api = Request.Builder()
+//            .url(url)
+//            .build()
+//
+//        val response = client.newCall(api).execute()
+//        return if (response.isSuccessful) {
+//            val json = response.body?.string() ?: ""
+//            gson.fromJson(json, ApiPollutionData::class.java)
+//        } else {
+//            throw Exception("Error: ${response.code}")
+//        }                                                     //with okHttp
+
+
     }
 
     interface PollutionApiService {
