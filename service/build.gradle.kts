283ba147ce3af8d0eabe87d9aa15318439bace2d
pooyaj21 <105147486+pooyaj21@users.noreply.github.com>
2023-10-23 14:13:12 +0330

    service module created

---
+++ service/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    kotlin("jvm")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("com.google.code.gson:gson:2.8.9")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(20)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,4 @@ plugins {
 }
 
 rootProject.name = "weatherApp"
+include("service")
